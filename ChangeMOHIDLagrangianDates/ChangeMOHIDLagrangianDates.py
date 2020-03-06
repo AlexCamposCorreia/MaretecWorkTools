0d26dd84d16ef3dc052db4b18a75cc75d557caa2
Alexandre Correia <alexandre.c.correia@tecnico.ulisboa.pt>
2020-03-06 18:02:40 +0000

    Update ChangeMOHIDLagrangianDates.py

--- ChangeMOHIDLagrangianDates/ChangeMOHIDLagrangianDates.py
+++ ChangeMOHIDLagrangianDates/ChangeMOHIDLagrangianDates.py
@@ -2,7 +2,7 @@
 # Author: Alexandre Correia / MARETEC
 # Email: alexandre.c.correia@tecnico.ulisboa.pt
 # Last update: 2002-03-06
-
+#
 # This script finds the lines that specify the start and end of a MOHIDLagrangian settings .xml file
 # and replaces the date values by what is specified in a .dat file.
 #
