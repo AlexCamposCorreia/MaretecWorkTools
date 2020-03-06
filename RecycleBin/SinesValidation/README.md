ddc8ad7acc434fe239141cd4474ec439c76077c7
Alexandre Correia <alexandre.c.correia@tecnico.ulisboa.pt>
2020-03-06 17:41:58 +0000

    Deleting outdated stuff

--- RecycleBin/SinesValidation/README.md
+++
@@ -1,2 +0,0 @@
-# SinesValiadtion
-Really messy code for validating PiscisMod Sines model, but it works. Should be revisited
--- RecycleBin/SinesValidation/common_functions/write_to_csv.py
+++
@@ -1,6 +0,0 @@
-
-def write_to_csv(filename, prop_name, depths, props):
-    with open(filename, 'w') as f:
-        f.write('#depth,' + prop_name + '\n')
-        for n in range(len(depths)):
-            f.write(str(depths[n])+','+str(props[n])+'\n')
